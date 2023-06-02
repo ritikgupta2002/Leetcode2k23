e44747d98e0d3e752edcd68c7133aa74b6be98ae
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-06-03 00:56:58 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (10.09%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(s==t) return true;
-        int p1=0;
-        int p2=0;
-
-        while(p1!=s.length()&&p2!=t.length()){
-            if(s[p1]==t[p2]){
-                p1++;
-                p2++;
-            }
-            else{
-                p2++;
-            }
+        unordered_map<char, vector<int>> m;
+        int prev=-1;
+        for(int i=0;i<t.length();i++){
+            m[t[i]].push_back(i);
         }
-
-        if(p1==s.length()) return true;
-        else   return false;
-
+        for(auto c : s ){
+           auto it = m.find(c);
+            if(it==m.end()){
+                return false;
+            }
+            auto vec=it->second;
+            int pos=upper_bound(vec.begin(),vec.end(),prev)-vec.begin();//to get the pos from vec array) 
+            if(pos==vec.size()){
+                return false;
+            }
+            prev=vec[pos];
+        }
+        return true;
     }
 };
+
+//follow up question time compleixty will be t.c:- 10^9(O(100)+log base 2 (10000));
+//t.c= 10^9(O(100+13.458))  t.c=10^9(O(113))=10^9*O(C) ~ O(10^9) 
