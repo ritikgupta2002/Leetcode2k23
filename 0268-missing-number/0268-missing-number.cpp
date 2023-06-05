c61c418e561eaf68dfbb338171c6abf8d38a994a
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-06-05 23:31:32 +0530

    Update 0268-missing-number.cpp

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-      unordered_map<int,int> m;
-      for(int i=0;i<nums.size();i++){
-          m[nums[i]]=i;//stored the keys in hashmap 
-      }
-      
-      int diff;
-      for(int i=0;i<nums.size();i++){
-          diff=nums.size()-nums[i];
-          if(m.find(diff)!=m.end()){
-              if(diff==0){
-                  diff++;
-              }
-              if(diff==2){
-                  diff--;
-              }
-              continue;
-          }
-          else break;
-      }
-     return diff;
-        
+        int res=nums.size();
+        for(int i=0;i<nums.size();i++){
+            res=res^i;
+            res=res^nums[i];
+        }
+        return res;
+
     }
 };
+// time complexity:- O(n)
+// space complexity:- O(1)
