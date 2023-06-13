de74484e50b1e9e20b1543c9055b43b90e4f5d5e
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-06-13 15:51:55 +0530

    Time: 55 ms (76.27%), Space: 40.9 MB (99.83%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -4,5 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    return arr.map(fn);
+    let result = [];
+    for(let i =0; i<arr.length;i++){
+        result.push(fn(arr[i],i));
+    }
+    return result;
 };
