5ebbfca37df2c21e173747e3d26311b0ca902abe
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-06-24 15:04:16 +0530

    Time: 97 ms (33.54%), Space: 15.6 MB (94.82%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
    vector<string> findRepeatedDnaSequences(string s) {
-        unordered_map<string,int>m;
-        vector<string>ans;
-        if(s.length()<10){
-            return ans;
-        }
-        for(int i=0;i<=s.length()-10;i++){
-            string st="";
-             for(int j=i;j<i+10;j++){
-                 st+=s[j];     
-             }
-             m[st]++;
-        }
+       unordered_map<char , int > bitmask ;
+       bitmask['A']=0;
+       bitmask['C']=1;
+       bitmask['G']=2;
+       bitmask['T']=3;
 
-        for (auto i = m.begin(); i != m.end(); i++)
-        {
-            if(i->second>=2){
-                ans.push_back(i->first);
-            }
-        }
-        return ans;
+       vector<string> ans;
+       unordered_set<int> unique;
+       unordered_set<int> repeated;
+
+       string sequence;
+       if(s.length()<=10){
+           return ans;
+       }
+       for(int i=0; i<=s.length()-10;i++){//O(N-10);
+           sequence=s.substr(i,10);
+
+           int bitsequence=0;
+
+           for(char ch: sequence){
+               bitsequence=(bitsequence<<2)|bitmask[ch];
+//left shifted by 2 because we can represent 0 , 1, 2, 3 in min 2 bits so we shift only 2 bits to left for constructing bitmask of the sequence .
+           }
+
+           if(unique.count(bitsequence)&&!repeated.count(bitsequence)){
+               ans.push_back(sequence);
+               repeated.insert(bitsequence);
+           }
+           else{
+               unique.insert(bitsequence);
+           }
+       }
+       return ans;
     }
 };
