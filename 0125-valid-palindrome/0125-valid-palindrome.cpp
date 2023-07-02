e215e2ca732a14d623575c4b30847011c1a9b809
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-07-02 18:40:00 +0530

    Time: 5 ms (67.28%), Space: 7.5 MB (53.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        stack<char> st1;
-        stack<char> st2;
+       int p1=0 ;
+       int p2=s.length()-1;
 
-        for(int i=0; i<s.length() ; i++ ){
-          if(isalpha(s[i])){
-              //want to remove nonalphabatic chars thats why
-              char ch= tolower(s[i]);
-              st1.push(ch);
-          }
-          if(isdigit(s[i])){
-              st1.push(s[i]);
-          }
-
-        }
-        for(int i=s.length(); i>=0 ; i-- ){
-          if(isalpha(s[i])){
-              //want to remove nonalphabatic chars thats why
-              char ch= tolower(s[i]);
-              st2.push(ch);
-          }
-          if(isdigit(s[i])){
-              st2.push(s[i]);
-          }
-
-        }
-
-        while(!st1.empty()){
-            if(st1.top()==st2.top()){
-                st1.pop();
-                st2.pop();
-            }
-            else{
-                return false;
-            }
-
-        }
-
-        return true;
-
+       while(p1<=p2){
+           if(!isalnum(s[p1])) {
+               p1++;
+               continue;
+           }
+           if(!isalnum(s[p2])){
+               p2--;
+               continue;
+           }
+           if(tolower(s[p1])==tolower(s[p2])){
+               p1++;
+               p2--;
+           }
+           else{
+               return false;
+           }
+           
+       }
+       return true;
     }
 };
