d3276d6365b737e175842d07ee005e485c8d1de5
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-08-06 02:34:53 +0530

    Time: 11 ms (99.17%), Space: 19.4 MB (11.03%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
-void moveZeroes(vector<int>& nums) {
-
-         int i = 0 ;
-         int j = 1;
-
-         while(j!=nums.size()){
-             if(nums[i]==0&&nums[j]==0){
-                 j++;
-                 continue;
-             }
-             if(nums[i]==0&&nums[j]!=0){
-                 swap(nums[i],nums[j]);
-             }
-             i++;
-             j++;
-
-         }
-
+    void moveZeroes(vector<int>& nums) {
+        int i=0 ;
+        int j=0 ;
+        while(j<nums.size()){
+            if(nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+            }
+            j++;
+        }
     }
 };
-
