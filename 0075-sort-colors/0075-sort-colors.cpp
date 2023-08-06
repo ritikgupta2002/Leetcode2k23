38f7ddb74e71c0ad3016dfa131ce43cf0fb58bba
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-08-06 20:44:40 +0530

    Time: 2 ms (47.13%), Space: 8.4 MB (7.52%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-       unordered_map<int,int> m;
-       for(auto i : nums){
-           if(m.find(i)==m.end()){
-                m[i]=1;
-           }
-           else m[i]++;
-       }
-       int j=0;
-       for(int i=0;i<3;i++){
-           while(m[i]--){
-             nums[j]=i;
-             j++;
-           }
-       }
-
-
+        map<int,int> m;
+        for(int i=0 ;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        int k=0;
+        for(int i=0;i<3;i++){
+            while(m[i]--){
+                nums[k++]=i;
+            }
+        }
 
     }
 };
-
-
-// time complexity:- O(n)+O(3)*O(300) = ~O(n)+O(900)
-// space complexity:-O(3);
