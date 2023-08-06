929e01beea8873a0a543e116103ce15e19187312
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-08-06 19:42:07 +0530

    Time: 36 ms (5%), Space: 286.2 MB (5%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
+
+    bool isSub(string s, string t , int m , int n ){
+       if(m==0) return true;
+       if(n==0) return false;
+       if(s[m-1]==t[n-1]) {
+           return isSub(s,t,m-1,n-1);
+       }
+       return isSub(s,t,m,n-1);
+    }
     bool isSubsequence(string s, string t) {
-        unordered_map<char, vector<int>> m;
-        int prev=-1;
-        for(int i=0;i<t.length();i++){
-            m[t[i]].push_back(i);
-        }
-        for(auto c : s ){
-           auto it = m.find(c);
-            if(it==m.end()){
-                return false;
-            }
-            auto vec=it->second;
-            int pos=upper_bound(vec.begin(),vec.end(),prev)-vec.begin();//to get the pos from vec array) 
-            if(pos==vec.size()){
-                return false;
-            }
-            prev=vec[pos];
-        }
-        return true;
+        return isSub(s,t,s.length(),t.length());
     }
 };
-
-//follow up question time compleixty will be t.c:- 10^9(O(100)*log base 2 (10000));
-//t.c= 10^9(O(100*13.458))  t.c=10^9(O(1300))=10^9*O(1300) ~ O(10^9) 
