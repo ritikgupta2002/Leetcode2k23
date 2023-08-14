10526e4a408c289b3db42b8260d1c4818117de54
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-08-15 00:19:04 +0530

    Time: 7 ms (66.21%), Space: 7 MB (32.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-       
-        unordered_map<char,char > m;
-            
-            for(int i=0;i<s.length();i++){
-                
-                if(m.find(s[i])==m.end()) 
-                {      
-                       for(auto it : m ){
-                           if(it.second==t[i]){
-                                return false;
-                           }                       
-                       }
-                       m[s[i]]=t[i];
-                      
-                }
-                else{
-                    if(m[s[i]]!=t[i]){
-                        return false;
-                    }
-                }
-                
-            }
+        unordered_map<char,char>m1;
+        unordered_map<char,char>m2;
+        if(s.length()!=t.length()) return false;
+        for(int i=0;i<t.length();i++){
+           if((m1.find(s[i])!=m1.end()&&m1[s[i]]!=t[i])||
+             (m2.find(t[i])!=m2.end()&&m2[t[i]]!=s[i])){
+               return false;
+           }
+           m1[s[i]]=t[i];
+           m2[t[i]]=s[i];
+        }
         return true;
     }
 };
-//time complexity :- O(s.length())*O(128)= ~O(s.length)
