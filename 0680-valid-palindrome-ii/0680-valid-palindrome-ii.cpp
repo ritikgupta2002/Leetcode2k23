ecfa14ead38afcc542bd68999c5c234f48c003e9
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-08-16 09:33:13 +0530

    Time: 63 ms (10.86%), Space: 24 MB (54.56%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool ispalindrome(string s , int p1, int p2){
-        while(p1<=p2){
-            if(s[p1]==s[p2]){
-                p1++;
-                p2--;
-            }
-            else return false;
-        }
-        return true;
-    }
-    bool validPalindrome(string s) {
-      int p1=0;
-      int p2=s.length()-1;
+  bool ispalindrome(string s , int p1, int p2){
       while(p1<=p2){
           if(s[p1]==s[p2]){
-              p1++;
-              p2--;
+          p1++;
+          p2--;
           }
           else{
-              return ispalindrome(s,p1+1,p2)||ispalindrome(s,p1,p2-1);
+              return false;
           }
       }
       return true;
+    }
+    bool validPalindrome(string s) {
+        int p1=0;
+        int p2=s.length()-1;
+        while(p1<=p2){
+          if(s[p1]==s[p2]){
+          p1++;
+          p2--;
+          }
+          else return ispalindrome(s,p1+1,p2)||ispalindrome(s,p1,p2-1);
+        }
+        return true;
     }
 };
