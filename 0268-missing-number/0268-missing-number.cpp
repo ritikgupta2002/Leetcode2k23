baf6c073eb303a68820121ad4c09fb0638f7ab21
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-08-28 00:46:54 +0530

    Time: 14 ms (78.97%), Space: 18 MB (13.72%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,33 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int res=nums.size();
-        for(int i=0;i<nums.size();i++){
-            res=res^i;
-            res=res^nums[i];
-        }
-        return res;
-
-    }
-};
-// time complexity:- O(n)
-// space complexity:- O(1)
-
-
-//second approach 
-
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-     
-       int total = nums.size()*(nums.size() + 1)/2;
-       int sum=0;
-  
-       for(auto i : nums){
-            sum+=i;
-       }
-       return total-sum;
- 
+     int sum=0;
+     int total=nums.size()*(nums.size()+1)/2;
+     for(int i=0 ;i<nums.size();i++){
+         sum+=nums[i];
+     }
+     return total-sum;
 
     }
 };
