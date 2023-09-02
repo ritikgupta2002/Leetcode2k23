633715feeef9f313cb076dc7d3e084cac4316bcf
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-09-02 20:00:40 +0530

    Time: 5 ms (30.1%), Space: 6.1 MB (6.24%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-     int divide(int dividend, int divisor) {
-
-        if(dividend == divisor) return 1;
-
-        bool isPositive = (dividend<0 == divisor<0);   
-         // if both are of same sign, answer is positive
-        unsigned int  a = abs(dividend);
-        unsigned int  b = abs(divisor);
-        unsigned int  ans = 0;
-        
-        while(a >= b){  
-            // while dividend is greater than or equal to divisor
-            short q = 0;
-            while(a > (b<<(q+1)))
+    int divide(int dividend, int divisor) {
+        if(dividend==divisor) return 1 ;
+        unsigned int d1=abs(dividend);
+        unsigned int d2=abs(divisor);
+        bool isPositive=(dividend<0==divisor<0);
+        short q;
+        unsigned int ans;
+        while(d1>=d2){
+            q=0;
+            while(d1>(d2<<q+1)){
                 q++;
-            ans += (1<<q);  
-            // add the power of 2 found to the answer
-            a = a - (b<<q);  
-            // reduce the dividend by divisor * power of 2 found
+            }
+            ans+=1<<q;
+            d1=d1-(d2<<q);
         }
-    
-        if(ans==1<<31&&isPositive){
-            return INT_MAX;
-        }
+        if(ans==1<<31&&isPositive) return INT_MAX;
         if(ans>1<<31&&isPositive==false) return INT_MIN;
         return isPositive?ans:-ans;
-
     }
 };
