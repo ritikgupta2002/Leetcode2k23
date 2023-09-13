00517f84f3c4c03dd072e362b25c58aa965dc123
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-09-13 10:23:00 +0530

    Time: 58 ms (80.03%), Space: 13.1 MB (79.09%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int i=0,j;
-        int l, r ;
+        int i=0,j,l,r;
         long long  goal;
-        int n=nums.size();
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        while(i<n-3){
+        while(i<nums.size()){
             j=i+1;
-            while(j<n-2){
-                goal=target-nums[i];
-                goal-=nums[j];
-                l=j+1;
-                r=n-1;
-                while(l<r){
-                    if(nums[l]+nums[r]<goal){
-                        l++;
-                    }
-                    else if(nums[l]+nums[r]>goal){
-                        r--;
-                    }
-                    else{
-                        vector<int> v={nums[i],nums[j],nums[l],nums[r]};
-                        ans.push_back(v);
-                        while(l<r && nums[l]==v[2]) l++;
-                        while(l<r && nums[r]==v[3]) r--;
+            while(j<nums.size()){
+            l=j+1;
+            r=nums.size()-1;
+            goal=target-nums[i];
+            goal-=nums[j];
+            while(l<r){
+               if(nums[l]+nums[r]>goal)r--; 
+               else if(nums[l]+nums[r]<goal)l++;
+               else{
+                   vector<int> v={nums[i],nums[j],nums[l],nums[r]};
+                   ans.push_back(v);
+                   while(l<r && nums[l]==v[2]) l++;
+                   while(l<r && nums[r]==v[3]) r--;
+               }
 
-                    }
-            
-                }
-                int temp=nums[j];
-                j++;
-                while(j<n-2&&temp==nums[j]) j++;
             }
-             int temp=nums[i];
-             i++;
-             while(i<n-3&&temp==nums[i]) i++;
-        }
+            int temp=nums[j];
+            j++;
+            while(j<nums.size()&&nums[j]==temp) j++;
 
+            }
+            int temp=nums[i];
+            i++;
+            while(i<nums.size()&&nums[i]==temp) i++;
+        }
         return ans;
     }
 };
-
