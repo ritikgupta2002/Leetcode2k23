234ea7d1f5905119b4deb6dc913c6edb15d1b4d1
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-09-13 09:56:10 +0530

    Time: 133 ms (63.13%), Space: 29.9 MB (42.56%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-      vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int start =0 ;
+        int l , r ,target;
         sort(nums.begin(),nums.end());
-        int start=0 ;
-        int j=0 ;
-        int l,r ;
-        int target;
         vector<vector<int>> ans;
-        while(start<nums.size()-2){
+        while(start<nums.size()){
             target=nums[start]*-1;
             l=start+1;
             r=nums.size()-1;
             while(l<r){
-               if(nums[l]+nums[r]>target){
-                   r--;
-               }else if(nums[l]+nums[r]<target){
-                   l++;
-               }
-               else{
-                   vector<int>v={nums[start],nums[l++],nums[r--]};
-                   ans.push_back(v);
-                   while(l<r && nums[l]==v[1]){
-                       l++;
-                   }
-                   while(l<r && nums[r]==v[2]){
-                       r--;
-                   }
-               }
+                if(nums[l]+nums[r]<target){
+                    l++;
+                }
+                else if (nums[l]+nums[r]>target)
+                {
+                    r--;
+                }
+                else{
+                    vector<int> v;
+                    v.push_back(nums[start]);
+                    v.push_back(nums[l]);
+                    v.push_back(nums[r]);
+                    ans.push_back(v);
+
+                    while(l<r&&nums[l]==v[1]) l++;
+                    while(l<r&&nums[r]==v[2]) r--;
+
+                }
             }
-            int temp=nums[start];
-            start++;
-            while(start<nums.size()-2&&temp==nums[start]){
+              int temp=nums[start];
                 start++;
-            }
+                while(start<nums.size()&&nums[start]==temp){
+                    start++;
+                }
         }
         return ans;
     }
