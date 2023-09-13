bd2e689ca1955154343e0130878fe5b7a9edfa6a
Ritik gupta <99651822+ritikgupta2002@users.noreply.github.com>
2023-09-13 08:14:46 +0530

    Time: 181 ms (86.97%), Space: 89.9 MB (65.32%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -3,22 +3,22 @@ public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         int i=0 ;
         int j=0 ;
-        long long  sum=0 ;
+        long long  sum=0;
         long long  maxi=INT_MIN;
-        unordered_map<int,int>m;
+        unordered_map<int,int> s ;
         while(j<nums.size()){
-            m[nums[j]]++;
-            sum+=nums[j];
-            if(j-i+1==k){
-               if(m.size()==k){
-                   maxi=max(maxi,sum);
-               }
-               m[nums[i]]--;
-               sum=sum-nums[i];
-               if(m[nums[i]]==0) m.erase(nums[i]);
-               i++;
-            }
-            j++;
+          s[nums[j]]++;
+          sum+=nums[j];
+          if(j-i+1==k){
+              if(s.size()==k){
+              maxi=max(maxi,sum);
+              }
+              s[nums[i]]--;
+              sum=sum-nums[i];
+              if(s[nums[i]]==0) s.erase(nums[i]);
+              i++;
+          }
+          j++;
         }
         return maxi==INT_MIN?0:maxi;
     }
